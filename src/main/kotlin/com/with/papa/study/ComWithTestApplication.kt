78c11e1f777e6ef5c6d05df10a8bfa1e9c3abab0
jclee@atoncorp.com <jclee@atoncorp.com>
2020-03-15 15:39:29 +0900

    개인 프로젝트 시작

---
+++ src/main/kotlin/com/with/papa/study/ComWithTestApplication.kt
@@ -0,0 +1,11 @@
+package com.with.papa.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComWithTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ComWithTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/with/papa/study/ComWithTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.with.papa.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComWithTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
